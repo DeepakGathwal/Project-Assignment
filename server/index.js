97d5a0963b8a88fd699e203a1f9c739837aff953
Deepak Gathwal <rickygathwal@gmail.com>
2023-11-23 12:03:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,37 +1,31 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const body = require('body-parser')
-const cors = require('cors')
-const connection = require('./conn/db')
-const dotenv = require('dotenv')
-const ErrorHandler = require('./middelwares/error')
-dotenv.config({path:'./config/.env'});
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const body = require("body-parser");
+const cors = require("cors");
+const connection = require("./conn/db");
+const dotenv = require("dotenv");
+const ErrorHandler = require("./middelwares/error");
+dotenv.config({ path: ".env" });
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(body.urlencoded({ extended: true }));
 
+app.use(cors({credentials:true, origin:"http://localhost:3000", methods: "GET,POST,PUT,DELETE",}))
 
-app.use(cors());
-app.use(
-  cors({
-    origin: "*",
-    allowedHeaders: "Authorization",
-  })
-);
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 connection();
-app.use("/upload", express.static("./upload"))
-const User = require('./routes/Userroute');
-const Posts = require('./routes/Postroute');
-app.use('/memories/user',User)
-app.use('/memories/post',Posts)
+app.use("/upload", express.static("./upload"));
+const User = require("./routes/Userroute");
+const Posts = require("./routes/Postroute");
+app.use("/memories/user", User);
+app.use("/memories/post", Posts);
 
-app.use(ErrorHandler)
+app.use(ErrorHandler);
 
-app.listen(process.env.PORT,() => {
-    console.log(`Your Server Running on ${process.env.PORT}`);
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Your Server Running on ${process.env.PORT}`);
+});

1812ebb5a81852002806f40993e713b29971a63f
Deepak Gathwal <rickygathwal@gmail.com>
2023-11-23 12:10:52 +0530

    Update URL.jsx

--- client/src/CommonUrl/URL.jsx
+++ client/src/CommonUrl/URL.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 const instance =  axios.create({baseURL:"https://gathwal-memories-2.onrender.com/memories/"})
-instance.defaults.headers.common['Content-Type'] = 'multipart/form-data';
-instance.defaults.withCredentials = true;
 
 export default instance;
 
